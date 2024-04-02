245e8c57e587a647471b10886a9b723abf47d70d
mindevis <mindevis.by@gmail.com>
2024-04-02 23:06:11 +0300

    Added term coloring for scripts

--- composes/percona-mysql/start.sh
+++ composes/percona-mysql/start.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+if  [ "$TERM" = "linux" ] || [ "$TERM" = "xterm" ] || [ "$TERM" = "xterm-color" ] || [ "$TERM" = "screen" ] && [ "$USE_COLORS" = "1" ]; then
+    WARN=$'\e[33;01m'
+    NORMAL=$'\e[0m'
+else
+    NORMAL=$'\e[0m'
+    WARN=$'\e[0;33m'
+fi
 
 echo -e "${WARN}Start Percona MySQL environment.${NORMAL}"
 $(which docker) compose up -d
